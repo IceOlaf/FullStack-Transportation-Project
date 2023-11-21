31fd12683070279edaf87fbd50be61e5bdc35161
IceOlaf <iceolaf0101@gmail.com>
2023-11-20 19:37:06 -0600

    Add sorting by time on getEventsByPersonId action

--- app/events_by_person_id/page.js
+++ app/events_by_person_id/page.js
@@ -13,7 +13,11 @@ export function SubmitButton() {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" aria-disabled={pending} className="p-2 px-5 border">
+    <button
+      type="submit"
+      aria-disabled={pending}
+      className="p-2 px-5 border text-center rounded-full"
+    >
       Search
     </button>
   );
@@ -25,26 +29,29 @@ const EventsByPersonIdPage = () => {
 
   console.log("Length of event", eventData?.length ?? 0);
   return (
-    <div className="bg-slate-800 min-h-screen text-white flex flex-col space-y-5">
+    <div className="bg-slate-800 min-h-screen text-white flex flex-col space-y-5 items-center">
       {/* Search box */}
-      <form action={formAction} className="">
+      <form action={formAction} className="mt-5 flex flex-col items-center">
         <label htmlFor="search">Search event by person Id</label>
         <input
           type="text"
           id="search"
           name="search"
-          className="text-black block"
+          placeholder="Search"
+          className="text-black py-2 px-5 block placeholder:px-2 focus:outline-none rounded-md mb-5"
           required
         />
-        <SubmitButton />
+        <div>
+          <SubmitButton />
+        </div>
       </form>
       {/* Cards to display event information */}
-      <div className="grid sm:grid-cols-3 md:grid-cols-4">
+      <div className="grid sm:grid-cols-3 md:grid-cols-4 gap-4">
         {eventData ? (
           eventData.map((event, i) => (
             <div
               key={i}
-              className="p-4 px-5 border flex flex-col justify-center "
+              className="p-4 px-5 border flex flex-col justify-center rounded-2xl shadow-lg" 
             >
               <p>{`Person:${event.person}`}</p>
               <p>{`Event type: ${event.type}`}</p>
@@ -52,7 +59,7 @@ const EventsByPersonIdPage = () => {
             </div>
           ))
         ) : (
-          <p>No event data</p>
+          <div className="flex justify-center items-center">No event data</div>
         )}
       </div>
     </div>
